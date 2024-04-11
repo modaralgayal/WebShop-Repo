b930bfa69e483961312c51447bb53b4e5de711b0
modar_algayal <mudar.algayal@helsinki.fi>
2024-04-11 14:41:46 +0300

    CSS work in UI product view positioning, not ready yet

--- Frontend/web-shop/src/Pages/viewProduct.tsx
+++ Frontend/web-shop/src/Pages/viewProduct.tsx
@@ -55,33 +55,34 @@ export const DisplayProductFullPage = () => {
   }
 
   return (
-    <div>
-      <p className="productName">{product.name}</p>
-      <p className="productDescription">{product.description}</p>
-      <p className="productPrice">{product.price},00 €</p>
-      <NativeSelect
-        className="sizes"
-        size="xl"
-        radius="lg"
-        data={['XS', 'S', 'M', 'L', 'XL']}
-        defaultValue="Select Size"
-      />
-      <Button
-        className="button"
-        variant="filled"
-        color="rgba(0, 0, 0, 1)"
-        radius="xs"
-        onClick={handleAddingProduct}
-      >
-        Add To Cart
-      </Button>
-      <div className="productCard">
-          <img
-            className="productImage"
-            src={`/productPng/${product.icon}`}
-            alt={product.name.toString()}
+    <div className="productContainer">
+      <div className="productInfo">
+          <p className="productName">{product.name}</p>
+          <p className="productDescription">{product.description}</p>
+          <p className="productPrice">{product.price},00 €</p>
+          <NativeSelect
+            className="sizes"
+            size="xl"
+            radius="lg"
+            data={['XS', 'S', 'M', 'L', 'XL']}
+            defaultValue="Select Size"
           />
-        </div>
+          <Button
+            className="button"
+            variant="filled"
+            color="rgba(0, 0, 0, 1)"
+            radius="xs"
+            onClick={handleAddingProduct}
+          >
+            Add To Cart
+          </Button>
+      </div>
+      <div className="productCard">
+        <img
+          className="productImage"
+          src={`/productPng/${product.icon}`}
+          alt={product.name.toString()}
+        />
+      </div>
     </div>
-  )
-}
+)}

cdbc0e1e600cc9ac9e36cb516913f0f2884cee49
modar_algayal <mudar.algayal@helsinki.fi>
2024-10-17 19:26:16 +0300

    edit

--- Frontend/web-shop/src/stripeComponents/checkoutForm.tsx
+++ Frontend/web-shop/src/stripeComponents/checkoutForm.tsx
@@ -29,7 +29,7 @@ export const CheckoutForm = ({ productIds }: { productIds: string[] }) => {
     const { error, paymentIntent } = await stripe.confirmPayment({
         elements,
         confirmParams: {
-            return_url: `${apiBaseUrl}/completion`,
+            return_url: `http://modarshop.online/completion`,
         },
         redirect: "if_required",
     });
@@ -48,7 +48,7 @@ export const CheckoutForm = ({ productIds }: { productIds: string[] }) => {
             });
 
             if (response.status === 200) {
-                window.location.href = `${apiBaseUrl}/completion`;
+                window.location.href = `http://modarshop.online/completion`;
             } else {
                 setMessage('Failed to add products to ordered.');
             }
